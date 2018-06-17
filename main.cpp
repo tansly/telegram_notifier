ed523194ced46d0ff22d96d0c8dc01e058e1997b
Yağmur Oymak <yagmur.oymak@gmail.com>
2018-06-17 23:40:54 +0300

    Receive messages asynchronously

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@
 #include "queue.h"
 
 #include <boost/asio.hpp>
+#include <future>
 #include <iostream>
+#include <memory>
 #include <string>
 #include <thread>
 #include <vector>
+
+/*
+ * TODO: Mutex on std::cout, std::cerr?
+ */
 
 static Queue<std::string> message_queue;
 
@@ -16,20 +22,33 @@ void receiver(void)
     using boost::asio::ip::tcp;
 
     boost::asio::io_context io_context;
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 6666));
-
-    std::string buf;
+    tcp::acceptor acceptor {io_context, tcp::endpoint(tcp::v4(), 6666)};
 
     for (;;) {
-        /*
-         * XXX: No error checking!
-         */
-        tcp::socket socket(io_context);
-        acceptor.accept(socket);
+        auto read_msg = [](std::unique_ptr<tcp::socket> socket_ptr)
+        {
+            std::string buf;
+            boost::system::error_code error;
 
-        size_t n = boost::asio::read_until(socket, boost::asio::dynamic_buffer(buf), "\n\n");
-        message_queue.enqueue(buf.substr(0, n));
-        buf.erase(0, n);
+            auto n = boost::asio::read(*socket_ptr, boost::asio::dynamic_buffer(buf), error);
+
+            if (!error || error == boost::asio::error::eof) {
+                message_queue.enqueue(buf);
+            } else {
+                /*
+                 * Some error occured that I don't want to handle.
+                 */
+                std::cerr << error.message();
+            }
+        };
+
+        auto socket_ptr = std::make_unique<tcp::socket>(io_context);
+
+        acceptor.accept(*socket_ptr);
+
+        std::packaged_task<void(std::unique_ptr<tcp::socket>)> task {read_msg};
+        std::thread task_td {std::move(task), std::move(socket_ptr)};
+        task_td.detach();
     }
 }
 
@@ -42,8 +61,8 @@ void transmitter(void)
 
 int main(int argc, char **argv)
 {
-    std::thread receive_thread{receiver};
-    std::thread transmit_thread{transmitter};
+    std::thread receive_thread {receiver};
+    std::thread transmit_thread {transmitter};
 
     receive_thread.join();
     transmit_thread.join();
